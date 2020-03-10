eacf176c82c4dd2f99a1b21579d4f9e9adccf6b1
Tobka <tobka777@gmail.com>
2020-03-10 10:57:46 +0300

    Solution Homework 1

--- hw/hw1/js/index.js
+++ hw/hw1/js/index.js
@@ -1,2 +1,16 @@
-const g = document.querySelector('g');
-const circle = document.querySelector('circle');
+const g = document.querySelector("g");
+const circle = document.querySelector("circle");
+const HAPPY = "Happy";
+const HAPPY_PATH = "M 150 200 Q 225 300 300 200";
+const SAD = "Sad";
+const SAD_PATH = "M 150 200 Q 225 100 300 200";
+
+function change() {
+  var text = document.getElementById("label_sad_happy").textContent;
+  var path = document.getElementById("smiley_path").getAttribute("d");
+  document.getElementById("label_sad_happy").textContent =
+    text == HAPPY ? SAD : HAPPY;
+  document
+    .getElementById("smiley_path")
+    .setAttribute("d", path == HAPPY_PATH ? SAD_PATH : HAPPY_PATH);
+}
